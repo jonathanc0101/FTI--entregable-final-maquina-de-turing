0aacf659e5379a00cbb1d88c9ddcc765d527fec1
jonathanc0101 <jonathancavia0101@gmail.com>
2021-11-17 21:58:37 -0300

    añadido archivo principal de test

--- codigo/clases.py
+++ codigo/clases.py
@@ -340,36 +340,4 @@ class maquinaDeTuring:
             i += 1
 
 
-def main():
-
-    chars = caracteresEspeciales()
-    grafo = GrafoTuring()
-
-    # inicializacion de la cinta 
-    universo = ["a", "b", "c", "*"]
-    pos = 0
-    cinta = "abababcbcbca*b*ababababb*"
-    MiCintaYCabezal = CintayCabezal(universo,cinta,pos)
-
-    ##inicializamos el grafo
-    grafo.insertar(1,2,chars.lambd,"S*")
-    grafo.insertar(2,3,chars.lambd,"R")
-    grafo.insertar(3,2,"!" + chars.delta, "D")
-    grafo.insertar(3,4,chars.delta, chars.lambd)
-    grafo.insertar(4,5,chars.lambd, "L")
-    grafo.insertar(5,4,"!*", chars.lambd)
-    grafo.insertar(5,6,"*","D")
-    grafo.insertar(6,7,chars.lambd,"END")
-
-    grafo.settearNodoInicial(1)
-
-    maquinaTuring = maquinaDeTuring(grafo,MiCintaYCabezal)
-    while True:
-        maquinaTuring.cinta.imprimirCintaYCabezal()
-        maquinaTuring.pasarUnTiempo()
-        time.sleep(0.1)
-
-if __name__ == "__main__":
-    main()
-
 
